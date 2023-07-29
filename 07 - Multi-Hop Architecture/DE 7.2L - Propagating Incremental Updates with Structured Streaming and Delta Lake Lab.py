b51b609841423c7eff4ce35aed00752b207a8e84
aldinei@gmail.com <aldinei@gmail.com>
2023-07-29 18:05:06 +0000

    Solved DE 7.2L

--- 07 - Multi-Hop Architecture/DE 7.2L - Propagating Incremental Updates with Structured Streaming and Delta Lake Lab.py
+++ 07 - Multi-Hop Architecture/DE 7.2L - Propagating Incremental Updates with Structured Streaming and Delta Lake Lab.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 # MAGIC %md-sandbox
-# MAGIC 
+# MAGIC
 # MAGIC <div style="text-align: center; line-height: 0; padding-top: 9px;">
 # MAGIC   <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning" style="width: 600px">
 # MAGIC </div>
@@ -8,10 +8,10 @@
 # COMMAND ----------
 
 # MAGIC %md <i18n value="0e458ce0-d210-41e2-8d96-836d7355de16"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC # Propagating Incremental Updates with Structured Streaming and Delta Lake
-# MAGIC 
+# MAGIC
 # MAGIC ## Learning Objectives
 # MAGIC By the end of this lab, you should be able to:
 # MAGIC * Apply your knowledge of structured streaming and Auto Loader to implement a simple multi-hop architecture
@@ -19,8 +19,8 @@
 # COMMAND ----------
 
 # MAGIC %md <i18n value="8cf2e657-3b59-4f53-a86e-6acabcd8aa16"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC ## Setup
 # MAGIC Run the following script to setup necessary variables and clear out past runs of this notebook. Note that re-executing this cell will allow you to start the lab over.
 
@@ -31,13 +31,13 @@
 # COMMAND ----------
 
 # MAGIC %md <i18n value="4d9563b5-a6dc-4106-a5bf-168d374a968e"/>
-# MAGIC 
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
+# MAGIC
 # MAGIC ## Ingest data
-# MAGIC 
+# MAGIC
 # MAGIC This lab uses a collection of customer-related CSV data from the **retail-org/customers** dataset.
-# MAGIC 
+# MAGIC
 # MAGIC Read this data using Auto Loader using its schema inference (use **`customers_checkpoint_path`** to store the schema info). Stream the raw data to a Delta table called **`bronze`**.
 
 # COMMAND ----------
@@ -48,10 +48,14 @@ dataset_source = f"{DA.paths.datasets}/retail-org/customers/"
 
 query = (spark
   .readStream
-  <FILL-IN>
+  .format("cloudFiles")
+  .option("cloudFiles.format", "csv")
+  .option("cloudFiles.schemaLocation", customers_checkpoint_path)
   .load(dataset_source)
   .writeStream
-  <FILL-IN>
+  .format("delta")
+  .option("checkpointLocation", customers_checkpoint_path)
+  .outputMode("append")
   .table("bronze")
 )
 
@@ -62,7 +66,7 @@ DA.block_until_stream_is_ready(query)
 # COMMAND ----------
 
 # MAGIC %md <i18n value="8b05f1fa-9046-4eba-8698-004b6c10fb79"/>
-# MAGIC 
+# MAGIC
 # MAGIC Run the cell below to check your work.
 
 # COMMAND ----------
@@ -74,9 +78,9 @@ assert spark.table("bronze").dtypes ==  [('customer_id', 'string'), ('tax_id', '
 # COMMAND ----------
 
 # MAGIC %md <i18n value="f999d572-86c1-4c0a-afbe-dad08cc7eb5a"/>
-# MAGIC 
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
+# MAGIC
 # MAGIC Let's create a streaming temporary view into the bronze table, so that we can perform transforms using SQL.
 
 # COMMAND ----------
@@ -89,10 +93,10 @@ assert spark.table("bronze").dtypes ==  [('customer_id', 'string'), ('tax_id', '
 # COMMAND ----------
 
 # MAGIC %md <i18n value="14a123fc-6cae-4780-b2fb-08ffa1da4989"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC ## Clean and enhance data
-# MAGIC 
+# MAGIC
 # MAGIC Using CTAS syntax, define a new streaming view called **`bronze_enhanced_temp`** that does the following:
 # MAGIC * Skips records with a null **`postcode`** (set to zero)
 # MAGIC * Inserts a column called **`receipt_time`** containing a current timestamp
@@ -101,15 +105,18 @@ assert spark.table("bronze").dtypes ==  [('customer_id', 'string'), ('tax_id', '
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC -- TODO
 # MAGIC CREATE OR REPLACE TEMPORARY VIEW bronze_enhanced_temp AS
 # MAGIC SELECT
-# MAGIC   <FILL-IN>
+# MAGIC   bronze_temp.*,
+# MAGIC   current_timestamp() as receipt_time,
+# MAGIC   input_file_name() as source_file
+# MAGIC   FROM bronze_temp
+# MAGIC   WHERE postcode > 0
 
 # COMMAND ----------
 
 # MAGIC %md <i18n value="4369381f-b2e0-4d88-9ccd-bcd6ff100f98"/>
-# MAGIC 
+# MAGIC
 # MAGIC Run the cell below to check your work.
 
 # COMMAND ----------
@@ -122,10 +129,10 @@ assert spark.table("bronze_enhanced_temp").isStreaming, "Not a streaming table"
 # COMMAND ----------
 
 # MAGIC %md <i18n value="ff38de01-61d2-4ffc-a5ca-aac815d8fb1e"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC ## Silver table
-# MAGIC 
+# MAGIC
 # MAGIC Stream the data from **`bronze_enhanced_temp`** to a table called **`silver`**.
 
 # COMMAND ----------
@@ -134,7 +141,10 @@ assert spark.table("bronze_enhanced_temp").isStreaming, "Not a streaming table"
 silver_checkpoint_path = f"{DA.paths.checkpoints}/silver"
 
 query = (spark.table("bronze_enhanced_temp")
-  <FILL-IN>
+  .writeStream
+  .format("delta")
+  .option("checkpointLocation", silver_checkpoint_path)
+  .outputMode("append")
   .table("silver"))
 
 # COMMAND ----------
@@ -144,7 +154,7 @@ DA.block_until_stream_is_ready(query)
 # COMMAND ----------
 
 # MAGIC %md <i18n value="41b4d01f-c855-40da-86a9-6d36de39d48a"/>
-# MAGIC 
+# MAGIC
 # MAGIC Run the cell below to check your work.
 
 # COMMAND ----------
@@ -157,9 +167,9 @@ assert spark.table("silver").filter("postcode <= 0").count() == 0, "Null postcod
 # COMMAND ----------
 
 # MAGIC %md <i18n value="e8fef108-ec3e-4404-9307-84cc3b593f99"/>
-# MAGIC 
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
+# MAGIC
 # MAGIC Let's create a streaming temporary view into the silver table, so that we can perform business-level aggregation using SQL.
 
 # COMMAND ----------
@@ -172,25 +182,27 @@ assert spark.table("silver").filter("postcode <= 0").count() == 0, "Null postcod
 # COMMAND ----------
 
 # MAGIC %md <i18n value="e37aaafc-d774-4635-9ca4-163446d98ac7"/>
-# MAGIC 
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
+# MAGIC
 # MAGIC ## Gold tables
-# MAGIC 
+# MAGIC
 # MAGIC Using CTAS syntax, define a new streaming view called **`customer_count_temp`** that counts customers per state in a column named **`customer_count`**.
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC -- TODO
 # MAGIC CREATE OR REPLACE TEMPORARY VIEW customer_count_temp AS
 # MAGIC SELECT 
-# MAGIC <FILL-IN>
+# MAGIC   state,
+# MAGIC   count(customer_id) AS customer_count
+# MAGIC FROM silver_temp
+# MAGIC GROUP BY state
 
 # COMMAND ----------
 
 # MAGIC %md <i18n value="7b1502ab-f64b-4491-8104-fa46521c4dc0"/>
-# MAGIC 
+# MAGIC
 # MAGIC Run the cell below to check your work.
 
 # COMMAND ----------
@@ -202,8 +214,8 @@ assert spark.table("customer_count_temp").dtypes ==  [('state', 'string'), ('cus
 # COMMAND ----------
 
 # MAGIC %md <i18n value="413fe655-e9c0-42ca-8bd9-e7f45c98d2ad"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC Finally, stream the data from the **`customer_count_temp`** view to a Delta table called **`gold_customer_count_by_state`**.
 
 # COMMAND ----------
@@ -214,7 +226,9 @@ customers_count_checkpoint_path = f"{DA.paths.checkpoints}/customers_counts"
 query = (spark
   .table("customer_count_temp")
   .writeStream
-  <FILL-IN>
+  .format("delta")
+  .option("checkpointLocation", customers_count_checkpoint_path)
+  .outputMode("complete")
   .table("gold_customer_count_by_state"))
 
 # COMMAND ----------
@@ -224,7 +238,7 @@ DA.block_until_stream_is_ready(query)
 # COMMAND ----------
 
 # MAGIC %md <i18n value="a4073245-d0e0-41a2-b107-16357064b596"/>
-# MAGIC 
+# MAGIC
 # MAGIC Run the cell below to check your work.
 
 # COMMAND ----------
@@ -237,11 +251,11 @@ assert spark.table("gold_customer_count_by_state").count() == 51, "Incorrect num
 # COMMAND ----------
 
 # MAGIC %md <i18n value="28b96faa-5b44-4e66-bc49-58ad9fa80f2e"/>
-# MAGIC 
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
+# MAGIC
 # MAGIC ## Query the results
-# MAGIC 
+# MAGIC
 # MAGIC Query the **`gold_customer_count_by_state`** table (this will not be a streaming query). Plot the results as a bar graph and also using the map plot.
 
 # COMMAND ----------
@@ -252,10 +266,10 @@ assert spark.table("gold_customer_count_by_state").count() == 51, "Incorrect num
 # COMMAND ----------
 
 # MAGIC %md <i18n value="c6bbef1b-0139-41c5-889b-f1cf1a56f907"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC ## Wrapping Up
-# MAGIC 
+# MAGIC
 # MAGIC Run the following cell to remove the database and all data associated with this lab.
 
 # COMMAND ----------
@@ -265,8 +279,8 @@ DA.cleanup()
 # COMMAND ----------
 
 # MAGIC %md <i18n value="971fb9e2-1315-47d1-bb18-ae0835b5bcde"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC By completing this lab, you should now feel comfortable:
 # MAGIC * Using PySpark to configure Auto Loader for incremental data ingestion
 # MAGIC * Using Spark SQL to aggregate streaming data
