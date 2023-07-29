158b240ed282a6419da058425ed6e0aa0470d1ab
aldinei@gmail.com <aldinei@gmail.com>
2023-07-29 18:48:40 +0000

    Solved DE 8.2.2

--- 08 - Delta Live Tables/DE 8.2 - DLT Lab/DE 8.2.1L - Lab Instructions.py
+++ 08 - Delta Live Tables/DE 8.2 - DLT Lab/DE 8.2.1L - Lab Instructions.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 # MAGIC %md-sandbox
-# MAGIC 
+# MAGIC
 # MAGIC <div style="text-align: center; line-height: 0; padding-top: 9px;">
 # MAGIC   <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning" style="width: 600px">
 # MAGIC </div>
@@ -8,12 +8,12 @@
 # COMMAND ----------
 
 # MAGIC %md <i18n value="2eb97b71-b2ab-4b68-afdc-1663ec49e9d4"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC # Lab: Migrating SQL Notebooks to Delta Live Tables
-# MAGIC 
+# MAGIC
 # MAGIC This notebook describes the overall structure for the lab exercise, configures the environment for the lab, provides simulated data streaming, and performs cleanup once you are done. A notebook like this is not typically needed in a production pipeline scenario.
-# MAGIC 
+# MAGIC
 # MAGIC ## Learning Objectives
 # MAGIC By the end of this lab, you should be able to:
 # MAGIC * Convert existing data pipelines to Delta Live Tables
@@ -21,25 +21,25 @@
 # COMMAND ----------
 
 # MAGIC %md <i18n value="782da0e9-5fc2-4deb-b7a4-939af49e38ed"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC ## Datasets Used
-# MAGIC 
+# MAGIC
 # MAGIC This demo uses simplified artificially generated medical data. The schema of our two datasets is represented below. Note that we will be manipulating these schema during various steps.
-# MAGIC 
+# MAGIC
 # MAGIC #### Recordings
 # MAGIC The main dataset uses heart rate recordings from medical devices delivered in the JSON format. 
-# MAGIC 
+# MAGIC
 # MAGIC | Field | Type |
 # MAGIC | --- | --- |
 # MAGIC | device_id | int |
 # MAGIC | mrn | long |
 # MAGIC | time | double |
 # MAGIC | heartrate | double |
-# MAGIC 
+# MAGIC
 # MAGIC #### PII
 # MAGIC These data will later be joined with a static table of patient information stored in an external system to identify patients by name.
-# MAGIC 
+# MAGIC
 # MAGIC | Field | Type |
 # MAGIC | --- | --- |
 # MAGIC | mrn | long |
@@ -48,10 +48,10 @@
 # COMMAND ----------
 
 # MAGIC %md <i18n value="b691e21b-24a5-46bc-97d8-a43e9ae6e268"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC ## Getting Started
-# MAGIC 
+# MAGIC
 # MAGIC Begin by running the following cell to configure the lab environment.
 
 # COMMAND ----------
@@ -61,11 +61,11 @@
 # COMMAND ----------
 
 # MAGIC %md <i18n value="c68290ac-56ad-4d6e-afec-b0a61c35386f"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC ## Land Initial Data
 # MAGIC Seed the landing zone with more data before proceeding.
-# MAGIC 
+# MAGIC
 # MAGIC You will re-run this command to land additional data later.
 
 # COMMAND ----------
@@ -75,8 +75,8 @@ DA.data_factory.load()
 # COMMAND ----------
 
 # MAGIC %md <i18n value="7cb98302-06c2-4384-bdf7-2260cbf2662d"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC Execute the following cell to print out values that will be used during the following configuration steps.
 
 # COMMAND ----------
@@ -85,11 +85,21 @@ DA.print_pipeline_config()
 
 # COMMAND ----------
 
+# For those that do not want to work through the exercise of creating the pipeline
+DA.create_pipeline()
+
+# COMMAND ----------
+
+# Start the pipeline and block until it has completed, canceled or failed
+DA.start_pipeline()
+
+# COMMAND ----------
+
 # MAGIC %md <i18n value="784d3bc4-5c4e-4ef8-ab56-3ebaa92238b0"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC ## Create and Configure a Pipeline
-# MAGIC 
+# MAGIC
 # MAGIC 1. Click the **Workflows** button on the sidebar.
 # MAGIC 1. Select the **Delta Live Tables** tab.
 # MAGIC 1. Click **Create Pipeline**.
@@ -109,7 +119,7 @@ DA.print_pipeline_config()
 # MAGIC 1. Check the **Use Photon Acceleration** box.
 # MAGIC 1. For **Channel**, select **Current**
 # MAGIC 1. For **Policy**, select the value provided in the cell above.
-# MAGIC 
+# MAGIC
 # MAGIC Finally, click **Create**.
 
 # COMMAND ----------
@@ -119,52 +129,52 @@ DA.validate_pipeline_config()
 # COMMAND ----------
 
 # MAGIC %md <i18n value="3340e93d-1fad-4549-bf79-ec239b1d59d4"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC ## Open and Complete DLT Pipeline Notebook
-# MAGIC 
+# MAGIC
 # MAGIC You will perform your work in the companion notebook [DE 8.2.2L - Migrating a SQL Pipeline to DLT Lab]($./DE 8.2.2L - Migrating a SQL Pipeline to DLT Lab),<br/>
 # MAGIC which you will ultimately deploy as a pipeline.
-# MAGIC 
+# MAGIC
 # MAGIC Open the Notebook and, following the guidelines provided therein, fill in the cells where prompted to<br/>
 # MAGIC implement a multi-hop architecture similar to the one we worked with in the previous section.
 
 # COMMAND ----------
 
 # MAGIC %md <i18n value="90a66079-16f8-4503-ab48-840cbdd07914"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC ## Run your Pipeline
-# MAGIC 
+# MAGIC
 # MAGIC Select **Development** mode, which accelerates the development lifecycle by reusing the same cluster across runs.<br/>
 # MAGIC It will also turn off automatic retries when jobs fail.
-# MAGIC 
+# MAGIC
 # MAGIC Click **Start** to begin the first update to your table.
-# MAGIC 
+# MAGIC
 # MAGIC Delta Live Tables will automatically deploy all the necessary infrastructure and resolve the dependencies between all datasets.
-# MAGIC 
+# MAGIC
 # MAGIC **NOTE**: The first table update may take several minutes as relationships are resolved and infrastructure deploys.
 
 # COMMAND ----------
 
 # MAGIC %md <i18n value="d1797d22-692c-43ce-b146-1e0248e65da3"/>
-# MAGIC 
-# MAGIC 
+# MAGIC
+# MAGIC
 # MAGIC ## Troubleshooting Code in Development Mode
-# MAGIC 
+# MAGIC
 # MAGIC Don't despair if your pipeline fails the first time. Delta Live Tables is in active development, and error messages are improving all the time.
-# MAGIC 
+# MAGIC
 # MAGIC Because relationships between tables are mapped as a DAG, error messages will often indicate that a dataset isn't found.
-# MAGIC 
+# MAGIC
 # MAGIC Let's consider our DAG below:
-# MAGIC 
+# MAGIC
 # MAGIC <img src="https://files.training.databricks.com/images/dlt-dag.png">
-# MAGIC 
+# MAGIC
 # MAGIC If the error message **`Dataset not found: 'recordings_parsed'`** is raised, there may be several culprits:
 # MAGIC 1. The logic defining **`recordings_parsed`** is invalid
 # MAGIC 1. There is an error reading from **`recordings_bronze`**
 # MAGIC 1. A typo exists in either **`recordings_parsed`** or **`recordings_bronze`**
-# MAGIC 
+# MAGIC
 # MAGIC The safest way to identify the culprit is to iteratively add table/view definitions back into your DAG starting from your initial ingestion tables. You can simply comment out later table/view definitions and uncomment these between runs.
 
 # COMMAND ----------
--- 08 - Delta Live Tables/DE 8.2 - DLT Lab/DE 8.2.2L - Migrating a SQL Pipeline to DLT Lab.sql
+++ 08 - Delta Live Tables/DE 8.2 - DLT Lab/DE 8.2.2L - Migrating a SQL Pipeline to DLT Lab.sql
@@ -1,6 +1,6 @@
 -- Databricks notebook source
 -- MAGIC %md-sandbox
--- MAGIC 
+-- MAGIC
 -- MAGIC <div style="text-align: center; line-height: 0; padding-top: 9px;">
 -- MAGIC   <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning" style="width: 600px">
 -- MAGIC </div>
@@ -8,70 +8,71 @@
 -- COMMAND ----------
 
 -- MAGIC %md <i18n value="cffedc4f-a6a4-4412-8193-46a7a8a6a213"/>
--- MAGIC 
--- MAGIC 
+-- MAGIC
+-- MAGIC
 -- MAGIC # Lab: Migrating a SQL Pipeline to Delta Live Tables
--- MAGIC 
+-- MAGIC
 -- MAGIC This notebook will be completed by you to implement a DLT pipeline using SQL. 
--- MAGIC 
+-- MAGIC
 -- MAGIC It is **not intended** to be executed interactively, but rather to be deployed as a pipeline once you have completed your changes.
--- MAGIC 
+-- MAGIC
 -- MAGIC To aid in completion of this Notebook, please refer to the <a href="https://docs.databricks.com/data-engineering/delta-live-tables/delta-live-tables-language-ref.html#sql" target="_blank">DLT syntax documentation</a>.
 
 -- COMMAND ----------
 
 -- MAGIC %md <i18n value="01e06565-56e7-4581-8833-14bc0db8c281"/>
--- MAGIC 
--- MAGIC 
+-- MAGIC
+-- MAGIC
 -- MAGIC ## Declare Bronze Table
--- MAGIC 
+-- MAGIC
 -- MAGIC Declare a bronze table, **`recordings_bronze`**, that ingests JSON data incrementally (using Auto Loader) from the simulated cloud source. The source location is already supplied as an argument; using this value is illustrated in the cell below.
--- MAGIC 
+-- MAGIC
 -- MAGIC As we did previously, include two additional columns:
 -- MAGIC * **`receipt_time`** that records a timestamp as returned by **`current_timestamp()`** 
 -- MAGIC * **`source_file`** that is obtained by **`input_file_name()`**
 
 -- COMMAND ----------
 
--- TODO
-CREATE <FILL-IN>
-AS SELECT <FILL-IN>
+CREATE OR REFRESH STREAMING LIVE TABLE recordings_bronze
+AS SELECT 
+  *,
+  current_timestamp() AS receipt_time,
+  input_file_name() AS source_file
   FROM cloud_files("${source}", "json", map("cloudFiles.schemaHints", "time DOUBLE, mrn INTEGER"))
 
 -- COMMAND ----------
 
 -- MAGIC %md <i18n value="57422f74-b830-4abb-b4a9-969d0ab90be6"/>
--- MAGIC 
--- MAGIC 
+-- MAGIC
+-- MAGIC
 -- MAGIC ### PII File
--- MAGIC 
+-- MAGIC
 -- MAGIC Using a similar CTAS syntax, create a live **table** into the CSV data found in the *healthcare/patient* dataset.
--- MAGIC 
+-- MAGIC
 -- MAGIC To properly configure Auto Loader for this source, you will need to specify the following additional parameters:
--- MAGIC 
+-- MAGIC
 -- MAGIC | option | value |
 -- MAGIC | --- | --- |
 -- MAGIC | **`header`** | **`true`** |
 -- MAGIC | **`cloudFiles.inferColumnTypes`** | **`true`** |
--- MAGIC 
+-- MAGIC
 -- MAGIC <img src="https://files.training.databricks.com/images/icon_note_24.png"/> Auto Loader configurations for CSV can be found <a href="https://docs.databricks.com/spark/latest/structured-streaming/auto-loader-csv.html" target="_blank">here</a>.
 
 -- COMMAND ----------
 
--- TODO
-CREATE <FILL-IN> pii
+CREATE OR REFRESH STREAMING LIVE TABLE  pii
 AS SELECT *
-  FROM cloud_files("${datasets_path}/healthcare/patient", "csv", map(<FILL-IN>))
+  FROM cloud_files("${datasets_path}/healthcare/patient", "csv", map("header", "true", "cloudFiles.inferColumnTypes", "true"))
 
 -- COMMAND ----------
 
 -- MAGIC %md <i18n value="3573b6a4-233a-4f23-a002-aab072eb5096"/>
--- MAGIC 
--- MAGIC 
+-- MAGIC
+-- MAGIC
 -- MAGIC ## Declare Silver Tables
--- MAGIC 
+-- MAGIC
 -- MAGIC Our silver table, **`recordings_enriched`**, will consist of the following fields:
--- MAGIC 
+-- MAGIC
 -- MAGIC | Field | Type |
 -- MAGIC | --- | --- |
 -- MAGIC | **`device_id`** | **`INTEGER`** |
@@ -79,33 +80,34 @@ AS SELECT *
 -- MAGIC | **`heartrate`** | **`DOUBLE`** |
 -- MAGIC | **`time`** | **`TIMESTAMP`** (example provided below) |
 -- MAGIC | **`name`** | **`STRING`** |
--- MAGIC 
+-- MAGIC
 -- MAGIC This query should also enrich the data through an inner join with the **`pii`** table on the common **`mrn`** field to obtain the name.
--- MAGIC 
+-- MAGIC
 -- MAGIC Implement quality control by applying a constraint to drop records with an invalid **`heartrate`** (that is, not greater than zero).
 
 -- COMMAND ----------
 
--- TODO
 CREATE OR REFRESH STREAMING LIVE TABLE recordings_enriched
-  (<FILL-IN add a constraint to drop records when heartrate ! > 0>)
+  (CONSTRAINT positive_heartrate EXPECT (heartrate > 0) ON VIOLATION DROP ROW)
 AS SELECT 
-  CAST(<FILL-IN>) device_id, 
-  <FILL-IN mrn>, 
-  <FILL-IN heartrate>, 
-  CAST(FROM_UNIXTIME(DOUBLE(time), 'yyyy-MM-dd HH:mm:ss') AS TIMESTAMP) time 
-  FROM STREAM(live.recordings_bronze)
-  <FILL-IN specify an inner join with the pii table on the mrn field>
+  CAST(r.device_id AS INTEGER) device_id, 
+  CAST(r.mrn AS LONG) mrn, 
+  CAST(r.heartrate AS DOUBLE) heartrate, 
+  CAST(FROM_UNIXTIME(DOUBLE(time), 'yyyy-MM-dd HH:mm:ss') AS TIMESTAMP) time,
+  p.name
+  FROM
+     STREAM(LIVE.recordings_bronze) r
+     INNER JOIN STREAM(LIVE.pii) p ON p.mrn = r.mrn
 
 -- COMMAND ----------
 
 -- MAGIC %md <i18n value="3b9309a8-9e1d-46a2-a0eb-e95fe698d23b"/>
--- MAGIC 
--- MAGIC 
+-- MAGIC
+-- MAGIC
 -- MAGIC ## Gold Table
--- MAGIC 
+-- MAGIC
 -- MAGIC Create a gold table, **`daily_patient_avg`**, that aggregates **`recordings_enriched`** by **`mrn`**, **`name`**, and **`date`** and delivers the following columns:
--- MAGIC 
+-- MAGIC
 -- MAGIC | Column name | Value |
 -- MAGIC | --- | --- |
 -- MAGIC | **`mrn`** | **`mrn`** from source |
@@ -115,10 +117,16 @@ AS SELECT
 
 -- COMMAND ----------
 
--- TODO
-CREATE <FILL-IN> daily_patient_avg
-  COMMENT <FILL-IN insert comment here>
-AS SELECT <FILL-IN>
+CREATE OR REFRESH STREAMING LIVE TABLE daily_patient_avg
+  COMMENT "Daily mean heartrate by patient"
+AS SELECT
+  mrn,
+  name,
+  DATE(time) `date`,
+  MEAN(heartrate) avg_heartrate
+FROM
+  STREAM(LIVE.recordings_enriched)
+GROUP BY mrn, name, DATE(time)
 
 -- COMMAND ----------
 
